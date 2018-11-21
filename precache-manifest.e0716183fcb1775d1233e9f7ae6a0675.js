e4d4e66b234be3611fa4695266ab260775337062
Ilya <ilyalutskiy@gmail.com>
2018-11-20 19:21:22 -0600

    Updates

---
+++ precache-manifest.e0716183fcb1775d1233e9f7ae6a0675.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85723662cacfb999b089",
+    "url": "/zooclick/static/js/runtime~main.85723662.js"
+  },
+  {
+    "revision": "a136f7898297111d20ef",
+    "url": "/zooclick/static/js/main.a136f789.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/zooclick/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "a136f7898297111d20ef",
+    "url": "/zooclick/static/css/main.dea902ec.chunk.css"
+  },
+  {
+    "revision": "a0c07a3592436b95f4eee57faef9189f",
+    "url": "/zooclick/index.html"
+  }
+];
